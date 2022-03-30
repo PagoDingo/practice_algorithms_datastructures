2044503b30b14c798be33a49d57d0c50dfb4a139
Daniel Archibald <danielarchibald@Daniels-MacBook-Pro.local>
2022-03-30 02:13:07 -0400

    3-30

---
+++ autocomplete.js
@@ -0,0 +1,19 @@
+function autocomplete(input, dictionary){
+    var input = input.replace(/[^a-z]/gi, '');
+    var completions = [];
+  
+      for (var i = 0; i < dictionary.length; ++i){
+        if (completions.length === 5){
+          return completions;
+        }
+        if (dictionary[i].toLowerCase().startsWith(input.toLowerCase())){
+          completions.push(dictionary[i]);
+        }
+      }
+    
+    
+  console.log("input: " + input)
+  console.log(completions)
+    return completions;
+}
+  
